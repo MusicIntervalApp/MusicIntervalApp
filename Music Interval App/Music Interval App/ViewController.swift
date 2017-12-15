8d576428e3f10944464c961249ed6c56b8082d19
tchin1 <31546755+tchin1@users.noreply.github.com>
2017-12-15 00:20:39 -0500

    changes

--- Music Interval App/Music Interval App/ViewController.swift
+++ Music Interval App/Music Interval App/ViewController.swift
@@ -15,6 +15,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func unwindToMain(unwindSegue: UIStoryboardSegue){
+        
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
