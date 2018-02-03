e695f2eb064a2452f825db2143858a290497f83a
tchin1 <31546755+tchin1@users.noreply.github.com>
2018-02-03 12:34:38 -0500

    change

---
+++ Music Interval App/Music Interval App/Pitches.swift
@@ -0,0 +1,12 @@
+//
+//  Pitches.swift
+//  Music Interval App
+//
+//  Created by user131306 on 2/3/18.
+//  Copyright © 2018 Maryville App Development. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+
