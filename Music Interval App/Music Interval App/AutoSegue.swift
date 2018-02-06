0d5ba1a688e5d945fb3a98b828ccc7d2dca50299
tchin1 <31546755+tchin1@users.noreply.github.com>
2018-02-06 00:33:53 -0500

    changes

---
+++ Music Interval App/Music Interval App/AutoSegue.swift
@@ -0,0 +1,45 @@
+//
+//  AutoSegue.swift
+//  Music Interval App
+//
+//  Created by user131306 on 2/5/18.
+//  Copyright © 2018 Maryville App Development. All rights reserved.
+//
+
+import UIKit
+
+class AutoSegue: UIStoryboardSegue {
+    
+//    override func perform() {
+//        auto()
+    }
+    
+    
+//    func auto() {
+//        let toViewController = self.destination
+//        let fromViewController = self.source
+//
+//        let containerView = fromViewController.view.superview
+//        let originalCenter = fromViewController.view.center
+//
+//        toViewController.view.transform = CGAffineTransform(scaleX: 0.05, y: 0.05)
+//        toViewController.view.center = originalCenter
+//
+//        containerView?.addSubview(toViewController.view)
+//
+//        UIView.animate(withDuration: 0.3, delay: 0, animations: {
+//            toViewController.view.transform = CGAffineTransform.identity
+//            }, completion: {success in
+//                fromViewController.present(toViewController, animated: false, completion: nil)
+//                })
+//            }
+//
+//    func autoTime() {
+//        let timer = Timer(timeInterval: 3.0, target: UIStoryboard, selector: Selector, userInfo: nil, repeats: false)
+//    }
+
+    
+    }
+
+
+
--- Music Interval App/Music Interval App/PreGameViewController.swift
+++ Music Interval App/Music Interval App/PreGameViewController.swift
@@ -9,15 +9,24 @@
 import UIKit
 
 class PreGameViewController: UIViewController {
-    var counter: Int = 1
-    var timer: Timer!
+//    var counter: Int = 1
+//    var timer: Timer!
     
     
     override func viewDidLoad() {
-            super.viewDidLoad()
-        
+        super.viewDidLoad()
+        perform(Selector("showGameView"), with: nil, afterDelay: 3)
 
     }
+    
+    func showGameView() {
+        performSegue(withIdentifier: "goToGame", sender: self)
+    }
+    
+//    func timeToMoveOn() {
+//        self.performSegue(withIdentifier: "goToGame", sender: self)
+//    }
+    
         
 //    override func prepare(for segue: levelTransitionsSegue, sender: Any?){
 //        let gameVC = segue.destination as! PreGameViewController
