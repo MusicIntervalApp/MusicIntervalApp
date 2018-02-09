8eaed4bb6fa3527ccf747f11c43daacb0865bb23
tchin1 <31546755+tchin1@users.noreply.github.com>
2018-02-09 10:10:05 -0500

    changes

--- Music Interval App/Music Interval App/AutoSegue.swift
+++ Music Interval App/Music Interval App/AutoSegue.swift
@@ -8,35 +8,17 @@
 
 import UIKit
 
-class AutoSegue: UIStoryboardSegue {
+class ViewController: UIViewController {
     
-//    override func perform() {
-//        auto()
-//    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let timer = NSTimer.scheduledTimerWithTimeInterval(3.0, target: self, select: #selector(timeToMoveOn), userinfo: nil, repeats: false)        <#code#>
+    }
     
-    
-//    func auto() {
-//        let toViewController = self.destination
-//        let fromViewController = self.source
-//
-//        let containerView = fromViewController.view.superview
-//        let originalCenter = fromViewController.view.center
-//
-//        toViewController.view.transform = CGAffineTransform(scaleX: 0.05, y: 0.05)
-//        toViewController.view.center = originalCenter
-//
-//        containerView?.addSubview(toViewController.view)
-//
-//        UIView.animate(withDuration: 0.3, delay: 0, animations: {
-//            toViewController.view.transform = CGAffineTransform.identity
-//            }, completion: {success in
-//                fromViewController.present(toViewController, animated: false, completion: nil)
-//                })
-//            }
-//
-//    func autoTime() {
-//        let timer = Timer(timeInterval: 3.0, target: UIStoryboard, selector: Selector, userInfo: nil, repeats: false)
-//    }
+    func timeToMoveOn() {
+        self.performSegue(withIdentifier:"goToMainUI", send: self)
+    }
 
     
     
--- Music Interval App/Music Interval App/GameViewController.swift
+++ Music Interval App/Music Interval App/GameViewController.swift
@@ -15,6 +15,9 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        do{
+            
+        }
         // Do any additional setup after loading the view.
     }
 
